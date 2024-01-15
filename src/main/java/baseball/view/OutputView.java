7861c460c22d95d73dbb9556881a165d274d5242
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-16 00:54:08 +0900

    feat : 출력 기능 구현
    - 숫자 입력 안내 출력, 게임 결과 출력

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,32 @@
 package baseball.view;
 
-public class OutputView {
+import java.util.StringJoiner;
 
+public class OutputView {
+    public void instruction() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public void gameResult(int ball, int strike) {
+        if (strike == 3) {
+            System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+        }
+        else if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+        }
+        else {
+            printBallStrike(ball, strike);
+        }
+    }
+
+    private void printBallStrike(int ball, int strike) {
+        StringJoiner sj = new StringJoiner(" ");
+        if (ball != 0) {
+            sj.add(Integer.toString(ball)+"볼");
+        }
+        if (strike != 0) {
+            sj.add(Integer.toString(strike)+"스트라이크");
+        }
+        System.out.println(sj);
+    }
 }
