ddad2ba6f04f4b593171ba2eef15b04feb6d9197
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-15 23:35:34 +0900

    feat : 입력 기능 구현
    - 맞출 숫자 입력 받기, 종료 여부 숫자 입력 받기
    - 입력 조건에 대한 예외 처리

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,51 @@
 package baseball.view;
-
+import camp.nextstep.edu.missionutils.Console;
 public class InputView {
+    public int userNumber() {
+        String str =  Console.readLine();
+        int number = 0;
+        try {
+            number = checkUserNumberError(str);
+        } catch (IllegalArgumentException e) {
+            System.exit(1);
+        }
+        return number;
+    }
 
+    public int exitNumber() {
+        String str = Console.readLine();
+        int number = 0;
+        try {
+            number = checkExitNumberError(str);
+        } catch (IllegalArgumentException e) {
+            System.exit(1);
+        }
+        return number;
+    }
+
+    private int checkUserNumberError(String str) {
+        try{
+            int number = Integer.parseInt(str);
+            if (number < 111 || number > 999) {
+                throw new IllegalArgumentException();
+            }
+            return number;
+        }
+        catch (NumberFormatException ex){
+            throw new IllegalArgumentException();
+        }
+    }
+
+    private int checkExitNumberError(String str) {
+        try{
+            int number = Integer.parseInt(str);
+            if (number != 1 && number != 2) {
+                throw new IllegalArgumentException();
+            }
+            return number;
+        }
+        catch (NumberFormatException ex){
+            throw new IllegalArgumentException();
+        }
+    }
 }
