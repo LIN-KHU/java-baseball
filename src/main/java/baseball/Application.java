eb4e682bb1ef4bf2293f7204b4222c483fccd234
SOOsuhyuncho <chosh0132@khu.ac.kr>
2024-01-16 17:37:43 +0900

    서로 다른 세 자리 랜덤 수 생성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,20 @@
 package baseball;
 
+import camp.nextstep.edu.missionutils.Console;
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
+        //computer - 서로 다른 세 자리 랜덤 수 생성(랜덤 수 생성 → 변수에 저장)
+    for (int i=0; i <3; i++){
+        randomNum = randomNum.concat(Integer.toString(getRandom()));
+    }}
+    public  static  String randomNum ="";
+    public static int getRandom()
+    {
+        return Randoms.pickNumberInRange(1, 10);
     }
+
 }
