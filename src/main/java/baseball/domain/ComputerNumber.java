ad5976f1fb955399dc334c3862aa4a0f03d5c494
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-01-17 15:47:32 +0900

    ComputerNumber 클래스 1차 완성

--- src/main/java/baseball/domain/ComputerNumber.java
+++ src/main/java/baseball/domain/ComputerNumber.java
@@ -1,4 +1,23 @@
 package baseball.domain;
 
+import camp.nextstep.edu.missionutils.Randoms;
+
 public class ComputerNumber {
+    private final int computerNumber;
+
+   public ComputerNumber() {
+       this.computerNumber = createThreeDigitNumberFromRandomNumbers();
+   }
+
+   private int createThreeDigitNumberFromRandomNumbers() {
+       return generateRandomNumber() * 100 + generateRandomNumber() * 10 + generateRandomNumber();
+   }
+
+   private int generateRandomNumber() {
+        return Randoms.pickNumberInRange(1, 9);
+   }
+
+   public int getComputerNumber() {
+       return this.computerNumber;
+   }
 }
