0efd76cb5197fb1bc62b82b6ee0ada980b1a9ef6
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 12:45:08 +0900

    Feat : 유저 입력 받는 메서드 추가
    스트링으로 입력 받고 검증 후 문제 없을 경우 입력값 반환

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,20 @@
 package baseball.view;
+import java.util.*;
+import camp.nextstep.edu.missionutils.Console;
+import static baseball.Application.validateInput;
 
 public class InputView {
+    public static String getUserInput() {
+        while(true) {
+            System.out.println("숫자를 입력해주세요 : ");
+            String userInput = Console.readLine();
+            try {
+                validateInput(userInput);
+                return userInput;
+            } catch (IllegalArgumentException error) {
+                System.out.println(error.getMessage());
+            }
+        }
+    }
 
 }
