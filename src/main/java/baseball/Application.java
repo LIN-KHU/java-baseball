44f7ccf027e619d0151d502f481951ecfc6c03d5
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 19:40:56 +0900

    Feat : createUserNumberList() 메서드 생성
    사용자 입력값 3자리 숫자를 ArrayList로 제작
    
    Feat : checkGameEndStatus() 메서드 생성
    
    게임이 끝나는지를 bool값으로 반환
    
    Feat : endGame() 메서드 생성
    
    새로운 게임을 시작할지 말지에 대해 프로그램 종료
    
    Refactor : validateInputNewGameStatus() 메서드 수정
    
    메서드 반환값 변경
    사용자의 입력값에 따라 프로그램 종료, 문제 없다면 1번 또는 2번에 따라 반환
    
    Refactor : countBallAndStrike() 메서드 수정
    
    사용자 입력값의 ArrayList와 컴퓨터의 ArrayList를 비교해서 볼과 스트라이크 개수 세기
    
    Refactor : createComputerNumber() 메서드 수정
    
    중복으로 두 숫자를 고르는 경우에 continue 때문에 3개까지 고른 뒤에 종료되도록 수정
    
    Refactor : main 메서드 분리
    
    컴퓨터는 난수를 한 게임당 한번 생성함으로 컴퓨터 난수 생성 후 게임 시작으로 처리
    그 후에 게임이 진행되고 게임이 끝나고 새로운 게임을 시작한다면 continue로 다시 반복문 실행
    
    Refactor : playGame() 메서드 수정
    
    숫자 입력 전 메시지 출력
    유저값을 받기
    유저값 확인
    ArrayList로 제작
    컴퓨터와 비교후 결과값 ArrayList로 제작하기
    게임이 끝나는지에 대해 bool값으로 처리
    게임이 끝나는지와 결과값을 가지고 적절한 결과 출력
    만약 게임이 끝난다면 새로운 게임 제작할지 묻고
    대답에 따라서 반복문 탈출 또는 프로그램 종료
    
    Refactor : validateUserNumberInput() 메서드 수정
    
    메서드 이름 변경
    메서드 반환값 변경
    경우에 따라 프로그램 종료

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -4,39 +4,75 @@ import camp.nextstep.edu.missionutils.Randoms;
 
 import static baseball.view.InputView.askContinueGame;
 import static baseball.view.InputView.getUserInput;
-import static baseball.view.OutputView.printRoundResult;
+import static baseball.view.OutputView.printIntroMessage;
+import static baseball.view.OutputView.printResult;
 
 public class Application {
     public static void main(String[] args) {
-        playGame();
-
+        while (true) {
+            ArrayList<Integer> computerNumberList = createComputerNumber();
+            Boolean newGame = playGame(computerNumberList);
+            if (newGame) {
+                continue;
+            }
+        }
     }
 
-    public static Boolean validateInput(String userInput) {
-        boolean inputStatus = (userInput.length() == 3);
-        for (int i = 0; i < 3; i++) {
-            if (userInput.charAt(i) == '0') {
-                inputStatus = false;
+    public static Boolean playGame(ArrayList<Integer> computerNumberList) {
+        while (true) {
+            printIntroMessage();
+            String stringUserInput = getUserInput();
+            validateUserNumberInput(stringUserInput);
+            ArrayList<Integer> userNumberList = createUserNumberList(stringUserInput);
+            ArrayList<Integer> resultList = countBallAndStrike(userNumberList, computerNumberList);
+            Boolean gameEndStatus = checkGameEndStatus(resultList);
+            printResult(gameEndStatus, resultList);
+            if (gameEndStatus) {
+                String userNewGameStartStatus = askContinueGame();
+                String newGameStartStatus = validateInputNewGameStatus(userNewGameStartStatus);
+                endGame(newGameStartStatus);
                 break;
             }
         }
-        return inputStatus;
+        return true;
+
+    }
+
+    public static void validateUserNumberInput(String userInput) {
+        if (userInput.length() != 3) {
+            System.exit(0);
+        }
+        for (int i = 0; i < 3; i++) {
+            if (userInput.charAt(i) == '0') {
+                System.exit(0);
+            }
+        }
+    }
+
+    public static ArrayList<Integer> createUserNumberList(String userInput) {
+        ArrayList<Integer> userNumberList = new ArrayList<>();
+        for (int i = 0; i < 3; i++) {
+            userNumberList.add((int) userInput.charAt(i) - '0');
+        }
+        return userNumberList;
     }
 
     public static ArrayList<Integer> createComputerNumber() {
         ArrayList<Integer> computerNumberList = new ArrayList<>();
-        for (int i = 0; i < 3; i++) {
-            int randomIntegerTypeNumber = Randoms.pickNumberInRange(1,9);
-            if (computerNumberList.contains(randomIntegerTypeNumber)) {
+        while (true) {
+            int randomIntegerNumber = Randoms.pickNumberInRange(1,9);
+            if (computerNumberList.contains(randomIntegerNumber)) {
                 continue;
             }
-            computerNumberList.add(randomIntegerTypeNumber);
+            computerNumberList.add(randomIntegerNumber);
+            if (computerNumberList.size() == 3) {
+                break;
+            }
         }
         return computerNumberList;
     }
 
-    public static ArrayList<Integer> countBallAndStrike() {
-        ArrayList<Integer> userNumberList = getUserInput(), computerNumberList = createComputerNumber();
+    public static ArrayList<Integer> countBallAndStrike(ArrayList<Integer> userNumberList, ArrayList<Integer> computerNumberList) {
         ArrayList<Integer> comparisonResult = new ArrayList<>();
         int ballNumber = 0, strikeNumber = 0;
         for (int i = 0; i < 3; i++) {
@@ -48,21 +84,29 @@ public class Application {
                 ballNumber++;
             }
         }
-        comparisonResult.add(ballNumber,strikeNumber);
+        comparisonResult.add(ballNumber);
+        comparisonResult.add(strikeNumber);
         return comparisonResult;
     }
 
-    public static Boolean validateInputNewGameStatus(String userInput) {
-        return userInput.equals("1") || userInput.equals("2");
+    public static Boolean checkGameEndStatus(ArrayList<Integer> resultList) {
+        return (resultList.get(1) == 3);
     }
 
-    public static void playGame() {
-        do {
-            getUserInput();
-            createComputerNumber();
-            countBallAndStrike();
-            printRoundResult();
-        } while (!printRoundResult().equals("2"));
+    public static String validateInputNewGameStatus(String userInput) {
+        String newGameStatus = "1";
+        if (!(userInput.equals("1") || userInput.equals("2"))) {
+            System.exit(0);
+        }
+        if (userInput.equals("2")) {
+            newGameStatus = "2";
+        }
+        return newGameStatus;
     }
 
+    public static void endGame(String startNewGameStatus) {
+        if (startNewGameStatus.equals("2")) {
+            System.exit(0);
+        }
+    }
 }
