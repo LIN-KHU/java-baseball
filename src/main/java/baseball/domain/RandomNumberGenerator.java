8e8e76dc84d5f7a5eb959ee31e29416a6b1cfc1d
Hyoseop Song <crad_on25@naver.com>
2024-01-18 23:59:50 +0900

    feat: 세 자리의 랜덤 난수를 생성하는 RandomNumberGenerator 클래스

---
+++ src/main/java/baseball/domain/RandomNumberGenerator.java
@@ -0,0 +1,28 @@
+package baseball.domain;
+
+import camp.nextstep.edu.missionutils.Randoms;
+import java.util.ArrayList;
+import java.util.List;
+
+public class RandomNumberGenerator {
+    public static final int START_INCLUSIVE = 1;
+    public static final int END_INCLUSIVE = 9;
+
+    public RandomNumberGenerator() {
+    }
+
+    public static List<Integer> makeRandomNumbers() {
+        List<Integer> randNumbers = new ArrayList<>();
+        do {
+            int randNum = makeRandomNumberInRange();
+            if (!randNumbers.contains(randNum)) {
+                randNumbers.add(randNum);
+            }
+        } while (randNumbers.size() < BallNumber.LENGTH);
+        return randNumbers;
+    }
+
+    public static int makeRandomNumberInRange() {
+        return Randoms.pickNumberInRange(START_INCLUSIVE, END_INCLUSIVE);
+    }
+}
