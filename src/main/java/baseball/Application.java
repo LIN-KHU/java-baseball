7bba02017ad857c604c3e7e5eea1b2031a0ee59e
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 12:51:17 +0900

    Feat : 사용자 입력값 검증 메서드 추가
    사용자의 입력값의 길이가 3, 즉 3자리 숫자가 아닐경우 에러 표시

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,16 @@
 package baseball;
+import baseball.view.InputView;
+import java.util.*;
 
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
+
+    }
+
+    public static void validateInput(String userInput) {
+        if (userInput.length() != 3) {
+                throw new IllegalArgumentException("[ERROR] 입력값은 3자리 숫자여야 합니다");
+        }
     }
 }
