8c4bd614f4784969b8973320bd879ad3eb6e3d40
Hyoseop Song <crad_on25@naver.com>
2024-01-19 03:18:37 +0900

    fix: 스트라이크, 볼 조건 판별 파라미터 수정

--- src/main/java/baseball/domain/ScoreCalculator.java
+++ src/main/java/baseball/domain/ScoreCalculator.java
@@ -5,18 +5,20 @@ import java.util.List;
 import java.util.stream.Stream;
 
 public class ScoreCalculator {
-    private final RandomNumberGenerator randomNumberGenerator;
+    BallNumber randomCreatedNumber;
 
     public ScoreCalculator(RandomNumberGenerator randomNumberGenerator) {
-        this.randomNumberGenerator = randomNumberGenerator;
+        List<Integer> integers = randomNumberGenerator.makeRandomNumbers();
+        this.randomCreatedNumber = new BallNumber(integers);
     }
 
-    public Score createScore(BallNumber ballNumber, BallNumber randomCreatedNumber) {
-        return new Score(countBall(ballNumber, randomCreatedNumber), countStrike(ballNumber, randomCreatedNumber));
+    public Score createScore(BallNumber ballNumber) {
+        return new Score(countStrike(ballNumber, randomCreatedNumber), countBall(ballNumber, randomCreatedNumber));
     }
 
     public int countBall(BallNumber ballNumber, BallNumber randomCreatedNumber) {
         return (int) Stream.iterate(0, i -> i + 1)
+                .limit(BallNumber.NUMBER_MAX_LENGTH)
                 .filter(i -> randomCreatedNumber.contains(ballNumber.getNumberAt(i)))
                 .filter(i -> ballNumber.getNumberAt(i) != randomCreatedNumber.getNumberAt(i))
                 .count();
@@ -24,11 +26,8 @@ public class ScoreCalculator {
 
     public int countStrike(BallNumber ballNumber, BallNumber randomCreatedNumber) {
         return (int) Stream.iterate(0, i -> i + 1)
+                .limit(BallNumber.NUMBER_MAX_LENGTH)
                 .filter(i -> ballNumber.getNumberAt(i) == randomCreatedNumber.getNumberAt(i))
                 .count();
-    }
-
-    public List<Integer> makeComputerNumber() {
-        return randomNumberGenerator.makeRandomNumbers();
     }
 }
