19fa0a1e8e058dadec31e400f1e750112f92074e
Jihyeon An <jh011007@naver.com>
2024-01-19 00:52:54 +0900

    computer util 생성

---
+++ src/main/java/baseball/ComputerUtil.java
@@ -0,0 +1,21 @@
+package baseball;
+
+import camp.nextstep.edu.missionutils.Randoms;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ComputerUtil {
+
+    public List<Integer> generateNumber() {
+
+        List<Integer> computerNumber = new ArrayList<>();
+
+        while(computerNumber.size() < 3) {
+            int number = Randoms.pickNumberInRange(1, 9);
+            if(!computerNumber.contains(number)) computerNumber.add(number);
+        }
+
+        return computerNumber;
+    }
+}
