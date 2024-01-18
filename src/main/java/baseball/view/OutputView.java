fe48fd907ac2cbae37e936c5ae61b3636b4613db
Hyoseop Song <crad_on25@naver.com>
2024-01-18 22:59:28 +0900

    feat: View interface 구현

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,13 @@
 package baseball.view;
 
-public class OutputView {
+import baseball.domain.JudgeResult;
 
+public interface OutputView {
+    void printInputNumber();
+
+    void printJudgeResult(JudgeResult judgeResult);
+
+    void printEndMessage();
+
+    void printRetryCommand();
 }
