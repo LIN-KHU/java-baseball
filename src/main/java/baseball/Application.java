8a39f3858aecd55c6094958bc3611f1438870167
Hyoseop Song <crad_on25@naver.com>
2024-01-19 00:55:47 +0900

    feat: GameClient 인스턴스 - 콘솔 입력 시작점 설정

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,23 @@
 package baseball;
 
+import baseball.controller.GameClient;
+import baseball.domain.RandomNumberGenerator;
+import baseball.domain.ScoreCalculator;
+import baseball.view.InputView;
+import baseball.view.InputViewImplement;
+import baseball.view.OutputView;
+import baseball.view.OutputViewImplement;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        InputView inputView = new InputViewImplement();
+        OutputView outputView = new OutputViewImplement();
+        RandomNumberGenerator randomNumberGenerator = new RandomNumberGenerator();
+        ScoreCalculator scoreCalculator = new ScoreCalculator(randomNumberGenerator);
+        GameClient gameClient = new GameClient(inputView, outputView, scoreCalculator);
+
+        do {
+            gameClient.run();
+        } while (gameClient.restart());
     }
 }
