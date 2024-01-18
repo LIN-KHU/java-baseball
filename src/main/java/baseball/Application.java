26312c1ec42e5637db2f500d9a4d0fcf3e29dd03
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 13:14:34 +0900

    Feat : 컴퓨터 난수 생성 메서드 추가
    pickNumberInRange(100,999)로 할 경우 중간에 0이 생기는 문제 발생
    반복문으로 세번 생성 후 문자열로 변환하여 생성하는 절차로 처리함

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,7 @@
 package baseball;
 import baseball.view.InputView;
 import java.util.*;
+import camp.nextstep.edu.missionutils.Randoms;
 
 public class Application {
     public static void main(String[] args) {
@@ -13,4 +14,15 @@ public class Application {
                 throw new IllegalArgumentException("[ERROR] 입력값은 3자리 숫자여야 합니다");
         }
     }
+
+    public static String createComputerNumber() {
+        String computerNumber = "";
+        for (int i = 0; i < 3; i++) {
+            Integer randomIntegerTypeNumber = Randoms.pickNumberInRange(1,9);
+            String randomStringTypeNumber = String.valueOf(randomIntegerTypeNumber);
+            computerNumber = computerNumber + randomStringTypeNumber;
+        }
+        return computerNumber;
+    }
+
 }
