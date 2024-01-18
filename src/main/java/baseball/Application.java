ff4195e09579025ce2bef3ddcef8adb1725207cf
unknown <ganghansoocut@gmail.com>
2024-01-18 21:08:46 +0900

    기능 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -10,18 +10,54 @@ public class Application {
         //TODO: 숫자 야구 게임 구현
 
         List<Integer> answer = makeAnswer();
+        Scanner scanner = new Scanner(System.in);
+        while (true) {
+            while (true) {
+                List<Integer> player = getNumbers();
+                int ball = 0;
+                int strike = 0;
+                List<Integer> com = makeAnswer();
 
-        boolean finish = false;
-        while (!finish) {
-            List<Integer> player = getNumbers();
-            int ball = 0;
-            int strike = 0;
-            for (int i = 0; i < 3; i++) {
-                if ()
+                if (player.get(0) == com.get(0)) strike++;
+                if (player.get(1) == com.get(1)) strike++;
+                if (player.get(2) == com.get(2)) strike++;
+
+                if (player.get(0) == com.get(1) || player.get(0) == com.get(2)) ball++;
+                if (player.get(1) == com.get(0) || player.get(1) == com.get(2)) ball++;
+                if (player.get(2) == com.get(0) || player.get(2) == com.get(1)) ball++;
+
+                if (strike == 3) {
+                    System.out.println("3스트라이크");
+                    System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+                    break;
+                } else {
+                    if (strike == 0 && ball == 0) {
+                        System.out.println("낫싱");
+                    } else if (strike != 0 && ball == 0) {
+                        System.out.printf("%d스트라이크\n", strike);
+                    } else if (strike == 0 && ball != 0) {
+                        System.out.printf("%d볼\n", ball);
+                    } else {
+                        System.out.printf("%d볼 %d스트라이크", ball, strike);
+                    }
+                }
+
+
+            }
+            // 예외처리 하기
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            boolean restart = scanner.nextBoolean();
+            if(!restart){
+                System.exit(0);
             }
         }
+
     }
 
+    // 예외처리 하기
+    // 1. 숫자이외의 입력
+    // 2. 3자리 이상 숫자의 입력
+    // 3. 각 자리의 숫자가 0이 아니고, 서로 같지 않아야 함
     public static List<Integer> getNumbers() {
 
         List<Integer> Numbers = new ArrayList<Integer>(3);
@@ -42,7 +78,7 @@ public class Application {
             first = random.nextInt(100) % 9 + 1;
             second = random.nextInt(100) % 9 + 1;
             third = random.nextInt(100) % 9 + 1;
-            if(!(first == second || second == third || third == first)){
+            if (!(first == second || second == third || third == first)) {
                 break;
             }
         }
