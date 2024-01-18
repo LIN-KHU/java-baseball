add2a431ebdb84e200a36783b64eb78e82916386
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 19:22:13 +0900

    Feat : printIntroMessage() 생성
    숫자를 입력하기 전에 출력하는 메시지 메서드로 생성
    
    Feat : printResult() 메서드 생성
    
    어떤 결과를 출력하는 지에 따라 printGameResult와 printRoundResult 중에서 고름
    
    Refactor : printRoundResult() 코드 수정
    
    게임이 끝나는 경우에 출력되는 문구를 제거
    배열을 받아오는 것을 매개변수로 받음
    
    Feat : printGameResult() 메서드 생성
    
    게임이 끝나는 경우에 출력되는 문구에 대한 코드 추가

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,30 +1,39 @@
 package baseball.view;
 import java.util.ArrayList;
-import static baseball.Application.countBallAndStrike;
-import static baseball.view.InputView.askContinueGame;
 
 public class OutputView {
-    public static String printRoundResult() {
-        String gameStatus = "3";
-        ArrayList<Integer> resultList = countBallAndStrike();
-        if (resultList.get(1) == 3) {
-            System.out.println(resultList.get(1) + "스트라이크");
-            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
-            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            gameStatus = askContinueGame();
+    public static void printIntroMessage() {
+        System.out.print("숫자를 입력해주세요 : ");
+    }
+
+    public static void printResult(Boolean gameEndStatus, ArrayList<Integer> resultList) {
+        if (gameEndStatus) {
+            printGameResult(resultList);
         }
-        if (resultList.get(0) == 0 && resultList.get(1) == 0) {
+        if (!gameEndStatus) {
+            printRoundResult(resultList);
+        }
+    }
+
+    public static void printRoundResult(ArrayList<Integer> resultList) {
+        if ((resultList.get(0) == 0) && (resultList.get(1) == 0)) {
             System.out.println("낫싱");
         }
-        if (resultList.get(0) == 0 && resultList.get(1) != 0) {
+        if ((resultList.get(0) == 0) && (resultList.get(1) != 0)) {
             System.out.println(resultList.get(1) + "스트라이크");
         }
-        if (resultList.get(0) != 0 && resultList.get(1) == 0) {
+        if ((resultList.get(0) != 0) && (resultList.get(1) == 0)) {
             System.out.println(resultList.get(1) + "볼");
         }
         if (resultList.get(0) != 0 && resultList.get(1) != 0) {
             System.out.println(resultList.get(0) + "볼 " + resultList.get(1) + "스트라이크");
         }
-        return gameStatus;
     }
+
+    public static void printGameResult(ArrayList<Integer> resultList) {
+        System.out.println(resultList.get(1) + "스트라이크");
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+    }
+
 }
