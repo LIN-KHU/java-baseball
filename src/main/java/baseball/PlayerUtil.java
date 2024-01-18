46b5584595de58b89d37bef470b175cc841241e8
Jihyeon An <jh011007@naver.com>
2024-01-19 00:53:10 +0900

    player util 생성

---
+++ src/main/java/baseball/PlayerUtil.java
@@ -0,0 +1,24 @@
+package baseball;
+
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import camp.nextstep.edu.missionutils.Console;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class PlayerUtil {
+
+    InputView inputView = new InputView();
+    OutputView outputView = new OutputView();
+
+    public List<Integer> generateNumber() {
+        outputView.printGetNumMessage();
+        return inputView.readPlayerNumber();
+    }
+
+    public int getRestartNumber() {
+        return inputView.readRestartNumber();
+    }
+
+}
