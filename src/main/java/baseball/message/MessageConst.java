37adc9868e6a8dfd1f20ac5ba918e39daa7381f7
Jihyeon An <jh011007@naver.com>
2024-01-19 00:53:34 +0900

    message 상수 생성

---
+++ src/main/java/baseball/message/MessageConst.java
@@ -0,0 +1,16 @@
+package baseball.message;
+
+public class MessageConst {
+
+    public static final String START_GAME_MSG = "숫자 야구 게임을 시작합니다.";
+
+    public static final String INPUT_MSG = "숫자를 입력해주세요 : ";
+
+    public static final String BALL = "볼";
+
+    public static final String STRIKE = "스트라이크";
+
+    public static final String NOTHING = "낫싱";
+
+    public static final String SUCCESS_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+}
