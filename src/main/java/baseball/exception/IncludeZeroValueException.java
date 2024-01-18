db983134d75ef284e12e4f9787bd9edd15cb7a2b
Hyoseop Song <crad_on25@naver.com>
2024-01-19 00:39:50 +0900

    fix: Custom exception이 message를 주입받지 않도록 수정

--- src/main/java/baseball/exception/IncludeZeroValueException.java
+++ src/main/java/baseball/exception/IncludeZeroValueException.java
@@ -1,7 +1,7 @@
 package baseball.exception;
 
 public class IncludeZeroValueException extends BaseballException{
-    protected IncludeZeroValueException(String errorMessage) {
+    public IncludeZeroValueException() {
         super("1~9 사이의 값을 입력해 주세요.");
     }
 }
