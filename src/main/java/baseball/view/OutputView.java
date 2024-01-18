e57ed31cde7bcbbc9cbc67ae1a3918b20d868f11
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 15:28:01 +0900

    Refactor : 메서드 내 코드 변경
    게임 진행 상태 값을 생성하고 종료 조건에서 사용자의 입력에 문제가 없다면 사용자의 입력값을 반환 그 외에는 반복될 수 있도록 조치

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -4,13 +4,14 @@ import static baseball.Application.countBallAndStrike;
 import static baseball.view.InputView.askContinueGame;
 
 public class OutputView {
-    public static void printRoundResult() {
+    public static String printRoundResult() {
+        String gameStatus = "3";
         ArrayList<Integer> resultList = countBallAndStrike();
         if (resultList.get(1) == 3) {
             System.out.println(resultList.get(1) + "스트라이크");
             System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
             System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
-            askContinueGame();
+            gameStatus = askContinueGame();
         }
         if (resultList.get(0) == 0 && resultList.get(1) == 0) {
             System.out.println("낫싱");
@@ -24,5 +25,6 @@ public class OutputView {
         if (resultList.get(0) != 0 && resultList.get(1) != 0) {
             System.out.println(resultList.get(0) + "볼 " + resultList.get(1) + "스트라이크");
         }
+        return gameStatus;
     }
 }
