0f49163dc12a0c85d44f94c26d75aea5a7886f09
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 14:53:27 +0900

    Refactor : 메서드 매개변수 제거
    메서드 내에서 호출하여 OutputView.java에서 제약없이 사용할 수 있음

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,6 +1,9 @@
 package baseball;
 import java.util.*;
 import camp.nextstep.edu.missionutils.Randoms;
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
+import static baseball.view.InputView.getUserInput;
 
 public class Application {
     public static void main(String[] args) {
@@ -26,7 +29,8 @@ public class Application {
         return computerNumber;
     }
 
-    public static ArrayList<Integer> countBallAndStrike(String userNumber, String computerNumber) {
+    public static ArrayList<Integer> countBallAndStrike() {
+        String userNumber = getUserInput(), computerNumber = createComputerNumber();
         ArrayList<Integer> comparisonResult = new ArrayList<>();
         int ballNumber = 0, strikeNumber = 0;
         for (int i = 0; i < 3; i++) {
