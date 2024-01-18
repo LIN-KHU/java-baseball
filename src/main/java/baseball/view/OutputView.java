f751e148d5fcef93906df10658d3784e22091432
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 15:09:04 +0900

    Feat : 결과 출력 메서드
    라운드별 결과 출력하기
    게임 종료시 게임 종료에 해당하는 메시지 출력 후 새게임 진행여부 메서드 호출

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,5 +1,28 @@
 package baseball.view;
+import java.util.ArrayList;
+import static baseball.Application.countBallAndStrike;
+import static baseball.view.InputView.askContinueGame;
 
 public class OutputView {
-
+    public static void printRoundResult() {
+        ArrayList<Integer> resultList = countBallAndStrike();
+        if (resultList.get(1) == 3) {
+            System.out.println(resultList.get(1) + "스트라이크");
+            System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+            System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            askContinueGame();
+        }
+        if (resultList.get(0) == 0 && resultList.get(1) == 0) {
+            System.out.println("낫싱");
+        }
+        if (resultList.get(0) == 0 && resultList.get(1) != 0) {
+            System.out.println(resultList.get(1) + "스트라이크");
+        }
+        if (resultList.get(0) != 0 && resultList.get(1) == 0) {
+            System.out.println(resultList.get(1) + "볼");
+        }
+        if (resultList.get(0) != 0 && resultList.get(1) != 0) {
+            System.out.println(resultList.get(0) + "볼 " + resultList.get(1) + "스트라이크");
+        }
+    }
 }
