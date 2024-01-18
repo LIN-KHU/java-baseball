e3dd71caf364375cc5c61b14bf29be0121e494b8
YeojoonHong <hyjhong2002@naver.com>
2024-01-18 19:18:20 +0900

    Refactor : getUserInput() 코드 수정
    getUserInput() 메서드 변환값 변경
    적절한 값인지, ArrayList로 변환하는 과정 제거
    출력메시지 제거, 입력만 받음
    
    Refactor : askContinueGame() 코드 수정
    
    적절한 값인지 판독하는 과정 제거
    입력만 받음

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,31 +1,14 @@
 package baseball.view;
-import java.util.*;
 import camp.nextstep.edu.missionutils.Console;
-import static baseball.Application.validateInput;
-import static baseball.Application.validateInputNewGameStatus;
+
 
 public class InputView {
-    public static ArrayList<Integer> getUserInput() {
-        ArrayList<Integer> userInputList = new ArrayList<>();
-        System.out.print("숫자를 입력해주세요 : ");
-        String userInput = Console.readLine();
-        boolean inputStatus = validateInput(userInput);
-        if (!inputStatus) {
-            System.exit(0);
-        }
-        for (int i = 0; i < 3; i++) {
-            userInputList.add((int) userInput.charAt(i));
-        }
-        return userInputList;
+    public static String getUserInput() {
+        return Console.readLine();
     }
 
     public static String askContinueGame() {
-        String continueStatus = Console.readLine();
-        Boolean gameStatus = validateInputNewGameStatus(continueStatus);
-        if (!gameStatus) {
-            System.exit(0);
-        }
-        return continueStatus;
+        return Console.readLine();
     }
 
 }
