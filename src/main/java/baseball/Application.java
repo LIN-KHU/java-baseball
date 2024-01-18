96b339db2c3bff4e799fc81e7a5af3b2750fa561
DESKTOP-F45SLBC\bnj30 <bjkim3059@gmail.com>
2024-01-18 17:42:18 +0900

    GameManager, Application 완성

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,29 +1,11 @@
 package baseball;
 
-import baseball.domain.CompareNumbers;
-import baseball.domain.Computer;
-import baseball.domain.User;
-import baseball.view.InputView;
-import baseball.view.OutputView;
-
-import java.util.List;
+import baseball.controller.GameManager;
 
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
-//        Computer computer = new Computer();
-//        int computerNumber = computer.getComputerNumber();
-//        System.out.println(computerNumber);
-//
-//        User user1 = new User();
-//
-//        user1.setUserNumber();
-//        int userNumber = user1.getUserNumber();
-//
-//        List<Integer> list = CompareNumbers.compareNumbers(computerNumber, userNumber);
-//
-//        OutputView.printBallAndStrikeMessage(list);
-//        OutputView.printThreeStrikeAndGameEnd(list);
-//        int restartNum = InputView.inputRestartOrNotNumber();
+        GameManager manager = new GameManager();
+        manager.gameStart();
     }
 }
--- src/main/java/baseball/controller/GameManager.java
+++ src/main/java/baseball/controller/GameManager.java
@@ -1,4 +1,44 @@
 package baseball.controller;
 
+import baseball.domain.CompareNumbers;
+import baseball.domain.Computer;
+import baseball.domain.User;
+import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.util.List;
+
+
 public class GameManager {
+
+    public GameManager() {
+
+    }
+
+    public void gameStart() {
+        Computer computer = new Computer();
+        int computerNumber = computer.getComputerNumber();
+
+        User user1 = new User();
+
+        List<Integer> list = userNumberInputAndCompareNumbersProcessingLogic(user1, computerNumber);
+
+        OutputView.printThreeStrikeAndGameEnd(list);
+        int restartNum = InputView.inputRestartOrNotNumber();
+        if (restartNum == 1) {
+            gameStart();
+        }
+    }
+
+    private List<Integer> userNumberInputAndCompareNumbersProcessingLogic(User user, int computerNumber) {
+        user.setUserNumber();
+        int userNumber = user.getUserNumber();
+        List<Integer> list = CompareNumbers.compareNumbers(computerNumber, userNumber);
+        OutputView.printBallAndStrikeMessage(list);
+        if (list.get(1) == 3) {
+            return list;
+        }
+        return userNumberInputAndCompareNumbersProcessingLogic(user, computerNumber);
+    }
+
 }
