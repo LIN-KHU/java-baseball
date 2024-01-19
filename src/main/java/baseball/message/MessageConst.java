b9f5497dce0bf6419417cbb390becdc29edf9bc5
Jihyeon An <jh011007@naver.com>
2024-01-19 15:52:20 +0900

    에러 메세지 생성

--- src/main/java/baseball/message/MessageConst.java
+++ src/main/java/baseball/message/MessageConst.java
@@ -2,8 +2,6 @@ package baseball.message;
 
 public class MessageConst {
 
-    public static final String START_GAME_MSG = "숫자 야구 게임을 시작합니다.";
-
     public static final String INPUT_MSG = "숫자를 입력해주세요 : ";
 
     public static final String BALL = "볼";
@@ -13,4 +11,10 @@ public class MessageConst {
     public static final String NOTHING = "낫싱";
 
     public static final String SUCCESS_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
+
+    public static final String ERROR_INPUT_OTHER_TYPE_MSG = "숫자를 입력해주세요.";
+
+    public static final String ERROR_INPUT_SIZE_MSG = "3자리의 숫자를 입력해주세요.";
+
+    public static final String ERROR_RESTART_INPUT_MSG = "1 또는 2 의 숫자만 입력 가능합니다.";
 }
