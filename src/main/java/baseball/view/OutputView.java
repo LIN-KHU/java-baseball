be57aa4e3d45330ebcab39cfd041002edf5a3575
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-20 16:45:52 +0900

    refactor : 분기문을 메소드 분리로 변경

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -10,15 +10,25 @@ public class OutputView {
     public void printGameResult(int ball, int strike) {
         if (strike == 3) {
             System.out.println("3스트라이크\n3개의 숫자를 모두 맞히셨습니다! 게임 종료\n게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
+            return;
         }
-        if (ball == 0 && strike == 0) {
-            System.out.println("낫싱");
-        }
-        if (ball > 0 || strike > 0){
-            printBallStrike(ball, strike);
-        }
+        printWhenNotGameOver(ball, strike);
     }
 
+    private void printWhenNotGameOver(int ball, int strike) {
+        if (ball > 0 || strike > 0){
+            printBallStrike(ball, strike);
+            return;
+        }
+        printWhenNothing(ball, strike);
+    }
+
+    private void printWhenNothing(int ball, int strike) {
+        if (ball == 0 && strike == 0) {
+            System.out.println("낫싱");
+            return;
+        }
+    }
     private void printBallStrike(int ball, int strike) {
         StringJoiner sj = new StringJoiner(" ");
         if (ball != 0) {
