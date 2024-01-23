57b1a94d6a17455258a9616c2c2dcadc7b77693c
Co-DDING <90135669+NoMyeongEun@users.noreply.github.com>
2024-01-24 03:24:24 +0900

    refactor : 볼, 스트라이크, 낫싱 메세지 필드로 지정

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -3,6 +3,9 @@ package baseball.view;
 import java.util.StringJoiner;
 
 public class OutputView {
+    private static final String ballMessage = "볼";
+    private static final String strikeMessage = "스트라이크";
+    private static final String nothingMessage = "낫싱";
     public void printGuessNumberInstruction() {
         System.out.print("숫자를 입력해주세요 : ");
     }
@@ -25,17 +28,17 @@ public class OutputView {
 
     private void printWhenNothing(int ball, int strike) {
         if (ball == 0 && strike == 0) {
-            System.out.println("낫싱");
+            System.out.println(nothingMessage);
             return;
         }
     }
     private void printBallStrike(int ball, int strike) {
         StringJoiner sj = new StringJoiner(" ");
         if (ball != 0) {
-            sj.add(Integer.toString(ball)+"볼");
+            sj.add(Integer.toString(ball)+ballMessage);
         }
         if (strike != 0) {
-            sj.add(Integer.toString(strike)+"스트라이크");
+            sj.add(Integer.toString(strike)+strikeMessage);
         }
         System.out.println(sj);
     }
