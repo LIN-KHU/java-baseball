996fa45bdca46feac6d260c64b710df64eb34a39
Jihyeon An <jh011007@naver.com>
2024-01-24 22:26:36 +0900

    입력 받는 기능한 수행하도록 수정

--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,32 +1,19 @@
 package baseball.view;
 
+import baseball.message.MessageConst;
 import camp.nextstep.edu.missionutils.Console;
 
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 
+import static baseball.message.MessageConst.*;
+
 public class InputView {
 
-    public List<Integer> readPlayerNumber() {
-
+    public String readPlayerNumber() {
         List<Integer> playerNumberList = new ArrayList<>();
-
-        String getPlayerNumber = Console.readLine();
-        /**
-         *  예외 1. 숫자가 아닌 다른 타입이 들어간 경우
-         * 예외 2. 중복된 숫자가 들어간 경우
-         * 예외 3. 세 자리 숫자가 아닌 경우
-         * */
-
-        String[] splitNumber = getPlayerNumber.split("");
-
-        for (int i = 0; i < 3; i++) {
-            int playerNumber = Integer.parseInt(splitNumber[i]);
-            playerNumberList.add(playerNumber);
-        }
-
-        return playerNumberList;
+        return Console.readLine();
     }
 
     public int readRestartNumber() {
