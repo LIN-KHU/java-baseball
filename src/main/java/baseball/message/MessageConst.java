3d2849add7aa94a5dfd230808c9842cbd89e1a22
Jihyeon An <jh011007@naver.com>
2024-01-24 22:26:49 +0900

    에러 메시지 수정

--- src/main/java/baseball/message/MessageConst.java
+++ src/main/java/baseball/message/MessageConst.java
@@ -12,7 +12,7 @@ public class MessageConst {
 
     public static final String SUCCESS_MSG = "3개의 숫자를 모두 맞히셨습니다! 게임 종료";
 
-    public static final String ERROR_INPUT_OTHER_TYPE_MSG = "숫자를 입력해주세요.";
+    public static final String ERROR_INPUT_TYPE_MSG = "숫자만 입력 가능합니다.";
 
     public static final String ERROR_INPUT_SIZE_MSG = "3자리의 숫자를 입력해주세요.";
 
