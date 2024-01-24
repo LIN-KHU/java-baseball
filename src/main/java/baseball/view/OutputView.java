a44e24e73eca85fc3a91371bdd9b98d4c54972d3
Jihyeon An <jh011007@naver.com>
2024-01-24 22:27:08 +0900

    에러 메시지 상수화

--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -1,8 +1,12 @@
 package baseball.view;
 
+import baseball.message.MessageConst;
+
+import static baseball.message.MessageConst.*;
+
 public class OutputView {
 
     public void printGetNumMessage() {
-        System.out.print("숫자를 입력해주세요 : ");
+        System.out.print(INPUT_MSG);
     }
 }
