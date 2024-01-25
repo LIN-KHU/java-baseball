a31c5e9814174d9c70606c118ebcb267a2be1e9d
김대욱 <55696059+MarriedSenior@users.noreply.github.com>
2024-01-25 13:09:18 +0900

    숫자 야구 게임 미션 1차 제출 (#5)

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,7 +1,85 @@
 package baseball;
 
+import baseball.view.InputView;
+import baseball.view.OutputView;
+import java.util.ArrayList;
+import java.util.List;
+
 public class Application {
     public static void main(String[] args) {
-        //TODO: 숫자 야구 게임 구현
+        run();
+    }
+
+    public static void run() {
+        while (true) {
+            ComputerNum com = new ComputerNum();
+            while (true) {
+                List<Integer> player = getNumbers();
+                int ball = 0;
+                int strike = 0;
+
+                strike = countStrike(com, player, strike);
+                ball = countBall(com, player, ball);
+
+                if (strike == 3) {
+                    printResultIfGameFinish();
+                    break;
+                }
+                printResultIfGameNotFinish(strike, ball);
+            }
+            restart();
+        }
+    }
+
+    public static List<Integer> getNumbers() {
+        InputView input = new InputView();
+        OutputView output = new OutputView();
+
+        List<Integer> numbers = new ArrayList<>();
+        output.printGetNumber();
+
+        int num = input.typeGetNumber();
+        while (num > 0) {
+            numbers.add(num % 10);
+            num /= 10;
+        }
+        return numbers;
+    }
+
+    public static int countStrike(ComputerNum com, List<Integer> player, int strike) {
+        if (player.get(0) == com.get(0)) strike++;
+        if (player.get(1) == com.get(1)) strike++;
+        if (player.get(2) == com.get(2)) strike++;
+
+        return strike;
+    }
+
+    public static int countBall(ComputerNum com, List<Integer> player, int ball) {
+        if ((player.get(0) == com.get(1)) || (player.get(0) == com.get(2))) ball++;
+        if ((player.get(1) == com.get(0)) || (player.get(1) == com.get(2))) ball++;
+        if ((player.get(2) == com.get(0)) || (player.get(2) == com.get(1))) ball++;
+
+        return ball;
+    }
+
+    public static void printResultIfGameFinish() {
+        OutputView output = new OutputView();
+        output.printResultIfGameFinish();
+    }
+
+    public static void printResultIfGameNotFinish(int strike, int ball) {
+        OutputView output = new OutputView();
+        output.printResultIfGameNotFinish(strike, ball);
+    }
+
+    public static void restart() {
+        InputView input = new InputView();
+        OutputView output = new OutputView();
+
+        output.printRestart();
+        int restart = input.typeRestartNumber();
+        if (restart == 2) {
+            System.exit(0);
+        }
     }
 }
--- src/main/java/baseball/view/InputView.java
+++ src/main/java/baseball/view/InputView.java
@@ -1,5 +1,25 @@
 package baseball.view;
 
+import camp.nextstep.edu.missionutils.Console;
+
 public class InputView {
+    public InputView(){}
+
+    // 예외처리 하기
+    // 1. 정수 타입을 입력하지 않을 때
+    // 2. 입력한 정수의 각 자리 숫자가 서로 다른 정수가 아닐 때
+    // 3. 1~9사이의 숫자가 아닐 때
+    // 4. 세자리 수가 아닐 때
+    public int typeGetNumber(){
+        return Integer.parseInt(Console.readLine());
+
+    }
+
+    // 예외처리하기
+    // 1. 정수 타입을 입력하지 않을 때
+    // 2. 1 혹은 2가 아닐 때
+    public int typeRestartNumber(){
+        return Integer.parseInt(Console.readLine());
+    }
 
 }
