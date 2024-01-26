2d5a6aab7e1be8c84d3abd1be7fcca4a94881615
Ojimin <ogm0205@gmail.com>
2024-01-26 10:05:04 +0900

    feat: 게임 종료 후 처리부분 구현

--- src/main/java/baseball/Application.java
+++ src/main/java/baseball/Application.java
@@ -1,16 +1,49 @@
 package baseball;
 
+import baseball.controller.BaseballGame;
 import baseball.model.ComputerNumber;
 import baseball.model.UserNumber;
 import baseball.view.InputView;
+import baseball.view.OutputView;
+
+import java.awt.*;
 
 public class Application {
     public static void main(String[] args) {
         //TODO: 숫자 야구 게임 구현
 //        BaseballGame baseballGame = new BaseballGame();
 //        baseballGame.start();
-        UserNumber inputUserNumber = new UserNumber(InputView.inputUserNumber());
         ComputerNumber computerNumber = new ComputerNumber();
-        System.out.println(computerNumber.getRandomNum());
+//        while (true) {
+//            //스트라이크, 볼 처리
+//            UserNumber inputUserNumber = new UserNumber(InputView.inputUserNumber());
+//
+//            //만약 다 맞추면 반복문 종료
+//
+//
+//        }
+        //1 or 2 입력 통해서 다시 게임 진행할지 종료할지 선택
+        OutputView.outputSuccessMessage();
+        OutputView.outputRestartMessage();
+        String inputUserRestartNumber = InputView.inputUserRestartNumber();
+        if(!inputUserRestartNumber.equals("1")  && !inputUserRestartNumber.equals("2")) {
+            throw new IllegalArgumentException("잘못된 입력입니다");
+        }
+        if(inputUserRestartNumber.equals("1")){
+            InputView.inputUserNumber();
+            ComputerNumber newComputerNumber = new ComputerNumber();
+            while (true) {
+                //스트라이크, 볼 처리
+                UserNumber inputUserNumber = new UserNumber(InputView.inputUserNumber());
+
+                //만약 다 맞추면 반복문 종료
+
+
+            }
+
+        }
+        if(inputUserRestartNumber.equals("2")){
+            return ;
+        }
     }
 }
--- src/main/java/baseball/view/OutputView.java
+++ src/main/java/baseball/view/OutputView.java
@@ -2,7 +2,11 @@ package baseball.view;
 
 public class OutputView {
 
-    public static void outputNumber() {
+    public static void outputSuccessMessage() {
+        System.out.println("3개의 숫자를 모두 맞히셨습니다! 게임 종료");
+    }
 
+    public static void outputRestartMessage() {
+        System.out.println("게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.");
     }
 }
